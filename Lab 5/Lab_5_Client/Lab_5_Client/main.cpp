16dc6825ae2da88d3247af8f1c57e98a81e2e6de
littekge <littekge@miamioh.edu>
2026-03-12 10:21:52 -0400

    feat: lab 5

---
+++ Lab 5/Lab_5_Client/Lab_5_Client/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Lab_5_Client.cpp
+ *
+ * Created: 3/12/2026 10:21:06 AM
+ * Author : Gabe Litteken, James Nippa
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    
+    while (1) 
+    {
+    }
+}
+
