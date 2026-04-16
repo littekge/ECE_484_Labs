58bbbbb28bdd20629b8525b677ffa26f8ecf7201
James Nippa <nippajd@miamioh.edu>
2026-04-16 10:36:19 -0400

    feat: lab 7

---
+++ Lab 7/Lab_7/Lab_7/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Lab_7.cpp
+ *
+ * Created: 4/16/2026 10:11:06 AM
+ * Author : james
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
